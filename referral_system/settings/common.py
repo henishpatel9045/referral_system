f7416506f05a6d22b201cb669f128d4bf8d26fea
Henish Patel <200120107182@git.org.in>
2024-04-08 08:58:53 +0530

    Add custom_auth.urls.py for handling authentication routes using JWT

--- referral_system/settings/common.py
+++ referral_system/settings/common.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_yasg",
     "custom_auth",
     "core",
 ]
@@ -116,3 +118,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "custom_auth.CustomUser"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
